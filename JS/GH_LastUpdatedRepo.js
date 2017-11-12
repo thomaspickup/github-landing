054f9b2b73ccbaab1f946f67d574514dc79521b3
Thomas Pickup <tpickup01@qub.ac.uk>
2017-11-12 02:40:09 +0000

    Latest repositories coded

--- JS/GH_LastUpdatedRepo.js
+++ JS/GH_LastUpdatedRepo.js
@@ -14,11 +14,27 @@ $(function setupLastUpdatedRepo(){
         });          
         
         function outputPageContent() {
-          if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
-          else {
-           // outhtml = outhtml + '<div class="row">'
-            outhtml = repositories[0].name + '<br>' + repositories[0].description + '<br>' + '<a href="' + repositories[0].html_url + '">Repository Link</a>'; 
-          }
+          if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p>'; }
+          else {  
+            outhtml = '<div class="card-deck">';
+            outhtml = outhtml + '<div class="row">';
+            for (i = 0; i < 2; i++) {
+                    outhtml = outhtml + '<div class="col-sm-10" height="100%"><div class="card" style="background-color:rgba(0, 0, 0, 0); border-color:rgba(0, 0, 0, 0)"><div class="card-body">';
+                    outhtml = outhtml + '<h4 class="card-title">';
+                    outhtml = outhtml + repositories[i].name;
+                    outhtml = outhtml + '</h4>';
+                    outhtml = outhtml + '<h6 class="card-subtitle mb-2 text-muted">';
+                    outhtml = outhtml + repositories[i].language;
+                    outhtml = outhtml + '</h6>';
+                    outhtml = outhtml + '<p class="card-text">';
+                    outhtml = outhtml + repositories[i].description;
+                    outhtml = outhtml + '</p>';
+                    outhtml = outhtml + '<a href="'+ repositories[i].html_url + '" class="btn">Repository</a>';
+                    outhtml = outhtml + '</div></div></div>';
+                }
+            outhtml = outhtml + '</div></div>';
+            }
+          
           $('#gh_lastupdatedrepo').html(outhtml);
         } // end outputPageContent()
     }); // end requestJSON Ajax call
